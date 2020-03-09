d5c0dbd62640da04000d5c026ae38461b0b2d1f5
walkor <walkor@users.noreply.github.com>
2020-03-09 18:28:29 +0800

    Update start_web.php

--- start_web.php
+++ start_web.php
@@ -11,20 +11,64 @@
  * @link http://www.workerman.net/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-use \Workerman\WebServer;
 use \Workerman\Worker;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use Workerman\Connection\TcpConnection;
 
-// 自动加载类
 require_once __DIR__ . '/vendor/autoload.php';
 
-// 这里使用workerman的WebServer运行Web目录。Web目录也可以用nginx/Apache等容器运行
-$web = new WebServer("http://0.0.0.0:8787");
+$web = new Worker("http://0.0.0.0:8787");
 
 $web->count = 2;
 
 $web->name = 'BrowserQuestWeb';
 
-$web->addRoot('www.your_domain.com', __DIR__.'/Web');
+define('WEBROOT', __DIR__ . '/Web');
+
+$web->onMessage = function (TcpConnection $connection, Request $request) {
+    $path = $request->path();
+    if ($path === '/') {
+        $connection->send(exec_php_file(WEBROOT.'/index.php'));
+        return;
+    }
+    $file = realpath(WEBROOT. $path);
+    if (false === $file) {
+        $connection->send(new Response(404, array(), '<h3>404 Not Found</h3>'));
+        return;
+    }
+    // Security check! Very important!!!
+    if (strpos($file, WEBROOT) !== 0) {
+        $connection->send(new Response(400));
+        return;
+    }
+    if (\pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+        $connection->send(exec_php_file($file));
+        return;
+    }
+
+    if (!empty($if_modified_since = $request->header('if-modified-since'))) {
+        // Check 304.
+        $info = \stat($file);
+        $modified_time = $info ? \date('D, d M Y H:i:s', $info['mtime']) . ' ' . \date_default_timezone_get() : '';
+        if ($modified_time === $if_modified_since) {
+            $connection->send(new Response(304));
+            return;
+        }
+    }
+    $connection->send((new Response())->withFile($file));
+};
+
+function exec_php_file($file) {
+    \ob_start();
+    // Try to include php file.
+    try {
+        include $file;
+    } catch (\Exception $e) {
+        echo $e;
+    }
+    return \ob_get_clean();
+}
 
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START'))
