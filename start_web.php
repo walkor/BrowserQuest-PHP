2b7687d7f393273b7079c3fc639d72cd501a6a6b
walkor <walkor@users.noreply.github.com>
2020-03-16 14:57:47 +0800

    Update start_web.php

--- start_web.php
+++ start_web.php
@@ -24,7 +24,7 @@ $web->count = 2;
 
 $web->name = 'BrowserQuestWeb';
 
-define('WEBROOT', __DIR__ . '/Web');
+define('WEBROOT',  __DIR__ . DIRECTORY_SEPARATOR . 'Web');
 
 $web->onMessage = function (TcpConnection $connection, Request $request) {
     $path = $request->path();
